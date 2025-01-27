4c352912a0d79d5c1708bc8c157bddc798b516e2
Aurélien Briot <briotaurelien88@gmail.com>
2025-01-27 11:57:47 +0100

    ajout de material sur la page de confirmation de rendez-vous

--- src/app/components/confirmation/confirmation.component.ts
+++ src/app/components/confirmation/confirmation.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {MatCardModule} from '@angular/material/card'; 
+import {MatButtonModule} from '@angular/material/button'; 
 
 @Component({
   selector: 'app-confirmation',
   standalone: true,
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './confirmation.component.html',
   styleUrls: ['./confirmation.component.scss'],
 })
