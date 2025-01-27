c6af45bb748bebcac72352a8c5f5a7c564d6681b
Aurélien Briot <briotaurelien88@gmail.com>
2025-01-27 10:28:25 +0100

    ajout de material sur la page de recherche

--- src/app/components/search-center/search-center.component.ts
+++ src/app/components/search-center/search-center.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import { MatButtonModule } from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-search-center',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatButtonModule, MatFormFieldModule, MatInputModule],
   templateUrl: './search-center.component.html',
   styleUrls: ['./search-center.component.scss'],
 })
